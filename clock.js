7c2e2327863a840c04348a3175da9693933867c8
Atilay <ati_sagofb@hotmail.com>
2022-05-08 00:32:13 +0300

    first commit

---
+++ clock.js
@@ -0,0 +1,23 @@
+
+
+const getGuestName = () => {
+  const name = prompt('Adınız nedir?', ' misafir');
+  document.querySelector('#prompt-name').innerText = myName;
+}
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('myClock').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+  getGuestName();
